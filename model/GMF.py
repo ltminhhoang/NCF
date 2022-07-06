9ccf01704c5b65d003cca3e28bc7aa2233879ea2
ltminhhoang <65478005+ltminhhoang@users.noreply.github.com>
2022-07-06 14:19:02 +0700

    Add files via upload

---
+++ model/GMF.py
@@ -0,0 +1,59 @@
+import torch.nn as nn
+import torch
+
+class GMF(nn.Module):
+    def __init__(self,
+                 num_users:int,
+                 num_items:int,
+                 num_factor:int=8,
+                 use_pretrain: bool = False,
+                 use_NeuMF:bool = False,
+                 pretrained_GMF=None
+                 ):
+        super(GMF,self).__init__()
+
+        self.pretrained_GMF = pretrained_GMF
+        self.num_users = num_users
+        self.num_items = num_items
+        self.use_pretrain = use_pretrain
+        self.use_NeuMF = use_NeuMF
+
+        self.pretrained_GMF = pretrained_GMF
+
+        self.user_embedding = nn.Embedding(num_users,num_factor)
+        self.item_embedding = nn.Embedding(num_items,num_factor)
+
+
+        self.predict_layer = nn.Linear(num_factor,1)
+        self.Sigmoid = nn.Sigmoid()
+
+        if use_pretrain:
+            self._load_pretrained_model()
+        else:
+            self._init_weight()
+
+    def _init_weight(self):
+        if not self.use_pretrain:
+            nn.init.normal_(self.user_embedding.weight,std=1e-2)
+            nn.init.normal_(self.item_embedding.weight,std=1e-2)
+        if not self.use_NeuMF:
+            nn.init.normal_(self.predict_layer.weight,std=1e-2)
+
+    def _load_pretrained_model(self):
+        self.user_embedding.weight.data.copy_(
+            self.pretrained_GMF.user_embedding.weight)
+        self.item_embedding.weight.data.copy_(
+            self.pretrained_GMF.item_embedding.weight)
+
+
+    def forward(self,users,items):
+        embedding_elementwise = self.user_embedding(users) * self.item_embedding(items)
+        if not self.use_NeuMF:
+            output = self.predict_layer(embedding_elementwise)
+            output = self.Sigmoid(output)
+            output = output.view(-1)
+        else:
+            output = embedding_elementwise
+
+        return output
+
---
+++ model/NeuMF.py
@@ -0,0 +1,54 @@
+import torch.nn as nn
+import torch
+from model.GMF import GMF
+from model.MLP import MLP
+
+class NeuMF(nn.Module):
+    def __init__(self,
+                 num_users: int,
+                 num_items: int,
+                 num_factor: int = 8,
+                 use_pretrain: bool = False,
+                 layer=None, # layer for MLP
+                 pretrained_GMF =None,
+                 pretrained_MLP =None
+                 ):
+        super(NeuMF,self).__init__()
+        self.use_pretrain = use_pretrain
+
+        self.pretrained_GMF = pretrained_GMF
+        self.pretrained_MLP = pretrained_MLP
+
+        # layer for MLP
+        if layer is None:
+            layer = [64,32, 16]
+        
+        self.predict_layer = nn.Linear(num_factor*2,1)
+        self.Sigmoid = nn.Sigmoid()
+
+        self.GMF = GMF(num_users, num_items, num_factor, use_pretrain=use_pretrain, use_NeuMF=True,
+                       pretrained_GMF=self.pretrained_GMF)
+        self.MLP = MLP(num_users, num_items, num_factor, layer, use_pretrain=use_pretrain, use_NeuMF=True,
+                       pretrained_MLP=self.pretrained_MLP)
+
+        if self.use_pretrain:
+            self._load_pretrain_model()
+
+
+        if not self.use_pretrain:
+            nn.init.normal_(self.predict_layer.weight,std=1e-2)
+
+    def _load_pretrain_model(self):
+        predict_weight = torch.cat([
+            self.pretrained_GMF.predict_layer.weight,
+            self.pretrained_MLP.predict_layer.weight], dim=1)
+        predict_bias = self.pretrained_GMF.predict_layer.bias + \
+                      self.pretrained_MLP.predict_layer.bias
+        self.predict_layer.weight.data.copy_(0.5 * predict_weight)
+        self.predict_layer.bias.data.copy_(0.5 * predict_bias)
+
+    def forward(self,user,item):
+        before_last_layer_output = torch.cat((self.GMF(user,item),self.MLP(user,item)),dim=-1)
+        output = self.predict_layer(before_last_layer_output)
+        output = self.Sigmoid(output)
+        return output.view(-1)
